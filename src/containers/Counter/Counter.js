3db8b5310974f86dacd02938024361aee2d712e5
mohammed <mohd.yaseen@tothenew.com>
2019-07-23 01:03:39 +0530

    reducer action dispatching

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,41 +1,49 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actionType from '../../store/actions';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+   
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storeResults.map(strResults => (
+                        <li onClick = {() => this.props.onDeleteResult(strResults.id)} key={strResults.id}>{strResults.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter,
+        storeResults : state.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: actionType.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionType.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionType.ADD,value:5}),
+        onSubCounter: () => dispatch({type: actionType.SUB,value:5}),
+        onStoreResult : () => dispatch({type: actionType.STORE_RESULT}),
+        onDeleteResult : (id) => dispatch({type: actionType.DELETE_RESULT,eleId : id})
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/store/reducers/result.js
@@ -0,0 +1,25 @@
+import * as actionType from '../actions';
+
+const initialState = {
+    results : []
+}
+
+const resultReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionType.STORE_RESULT:
+            return {
+                ...state,
+               results : state.results.concat({id : new Date(),value : state.counter})
+            }
+            case actionType.DELETE_RESULT:
+                const updatedArray = state.results.filter((ele) => ele.id !== action.eleId)
+            return {
+                ...state,
+               results : updatedArray
+            }
+            
+    }
+     return state;
+};
+
+export default resultReducer;
